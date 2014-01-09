ce2f018c49aa1c2f84e85d9e5e8ce2c79ccede2c
Brittan McGinnis <konamax123@gmail.com>
2014-01-09 06:56:43 -0800

    added spec and a couple lib folders

---
+++ spec/lib/hero_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative '../../lib/hero'
+
+describe Hero do
+
+	describe "default attributes" do
+		let(:hero) {Hero.new}
+
+		it "has default strength equal to 3" do
+			expect(hero.strength).to eq(3)
+		end
+		it "has default health equal to 10"
+
+	end
+
+end
