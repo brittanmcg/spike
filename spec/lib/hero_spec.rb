db2f84714ccc33dad3f9c681ea1848b740b50f35
Brittan McGinnis <konamax123@gmail.com>
2014-01-09 07:14:27 -0800

    added some tests and initialize method

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -9,8 +9,20 @@ describe Hero do
 		it "has default strength equal to 3" do
 			expect(hero.strength).to eq(3)
 		end
-		it "has default health equal to 10"
-
+		it "has default health equal to 10" do
+			expect(hero.health).to eq(10)
+		end
 	end
+
+	it "can be initialized with custom strength" do
+		hero = Hero.new strength: 3
+		expect(hero.strength).to eq(3)
+	end
+
+	it "can be initialized with custom health" do
+		hero = Hero.new health: 8
+		expect(hero.health).to eq(8)
+	end
+
 
 end
